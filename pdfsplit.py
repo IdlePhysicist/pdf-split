599852b4b3674dcceacb5921c407dbe1ee9dee9d
IdlePhysicist <eoghan.conlononeill@gmail.com>
2019-03-09 08:11:13 -0700

    Initial commit to quartz gui branch

--- pdfsplit.py
+++ pdfsplit.py
@@ -1,4 +1,93 @@
 #!/usr/bin/python
+# coding=utf-8
+"""
+
+PDFSplit v0.3 (Quartz GUI)
+IdlePhysicist, 2019
+
+This code is an amalgamation of Ben Byram-Wigfield's SPLITPDF v2.0 which I use to learn to PDFKit Framework,
+and my own ideas from my previous iterations of PDFSplit.
+
+This will operate in a Automator workflow, which calls this Python script, which will in turn call an
+AppleScript used to display a pop up window to as the user for input instuctions as to what functions the Python
+script is to carry out.
+
+Options (~Args):
+    - Range of pages: -r <start page>-<end page>
+    - Individual pages: -i [last page you want individually]
+
+For option 2, the default will be to split every page to a new PDF doc in a new directory. Directory behaveiour
+only occurs when >1 PDFs are being created.
+
+"""
+import os, sys
+import Quartz as Quartz
+from LaunchServices import (kUTTypeJPEG, kUTTypeTIFF, kUTTypePNG, kCFAllocatorDefault)
+from CoreFoundation import (CFAttributedStringCreate, CFURLCreateFromFileSystemRepresentation, NSURL)
+
+APPLE_SCRIPT = """
+    
+"""
+
+class App(object):
+    def __init__(self):
+        pass
+
+    def main(self):
+        for filename in sys.argv[1:]:
+            strip(filename)
+
+    def createPDFDocumentWithPath(self,path):
+        path = path.decode('utf-8')
+        pdfURL = NSURL.fileURLWithPath_(path)
+        if pdfURL:
+            return Quartz.PDFDocument.alloc().initWithURL_(pdfURL)
+
+    def getFilename(self, filepath):
+        i=0
+        newName = filepath
+        while os.path.exists(newName):
+            i += 1
+            newName = filepath + " %02d"%i
+        return newName
+
+    def strip(self,filename):
+        #
+        pdf = createPDFDocumentWithPath(filename)
+        numPages = pdf.pageCount()
+        shortName = os.path.splitext(filename)[0]
+        prefix = os.path.splitext(os.path.basename(filename))[0]
+        metaDict = pdf.documentAttributes()
+        folderName = getFilename(shortName)
+        try:
+            os.mkdir(folderName)
+        except:
+            print "Can't create directory '%s'"%(folderName)
+            sys.exit()
+
+        # For each page, create a file. Index starts at ZERO!!!
+        # You won't get leading zeros in filenames beyond 99.
+        for i in range (1, numPages+1):
+            page = pdf.pageAtIndex_(i-1)
+            if page:
+                newDoc = Quartz.PDFDocument.alloc().initWithData_(page.dataRepresentation())
+                outFile = folderName +"/" + prefix + " %03d.pdf"%i
+                newDoc.writeToFile_withOptions_(outFile, metaDict)
+
+if __name__ == "__main__":
+    App().main()
+
+
+
+
+
+
+
+
+
+'''
+
+#!/usr/bin/python
 
 import os
 import argparse
@@ -22,3 +111,4 @@ if not args.i:
     gs(input_pdf, page_range[0], page_range[1])
 else:
     for page in xrange(1, args.i + 1): gs(input_pdf, page, page)
+'''
